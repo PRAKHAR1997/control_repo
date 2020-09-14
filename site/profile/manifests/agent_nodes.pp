66e15e71cf1af4b6f86542d28e916e052bf93921
PRAKHAR GUPTA <PRAKHARGUPTA997@GMAIL.COM>
2020-09-15 03:04:55 +0530

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -2,7 +2,8 @@ class profile::agent_nodes{
   include dockeragent
   dockeragent::node {'web.puppet.vm':}
   dockeragent::node {'db.puppet.vm':}
-  host {'web.puppet.vm':
+  dockeragent::node {'minetest.puppet.vm':}
+host {'web.puppet.vm':
     ensure => present,
     ip => '172.18.0.3',
   }
@@ -10,4 +11,5 @@ class profile::agent_nodes{
     ensure => present,
     ip => '172.18.0.2',
   }
+  
 }
